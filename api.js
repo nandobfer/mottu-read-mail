d3296eb441aaff0efca1d4425217653f5c3f695c
nandobfer <nandobfer@yahoo.com.br>
2022-12-02 10:51:57 -0300

    update: https

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const api = axios.create({
-    baseURL: "http://localhost:4000/api/v1/bapka/mottu",
+    baseURL: "https://localhost:4000/api/v1/bapka/mottu",
     timeout: 1000 * 10,
 })
 
