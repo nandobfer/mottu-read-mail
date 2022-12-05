977ae415469a2f26c00deb7f2693ef09c012137b
nandobfer <nandobfer@yahoo.com.br>
2022-12-05 08:24:39 -0300

    test: mail split subject

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { api } = require('./api');
 const bootstrap = async () => {
     console.log('Fetching the new mail', new Date().toString());
     const mail = await readMail();
+    console.log(mail)
     const { loja, address } = processEmail(mail)
     console.log(address)
 
