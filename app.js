8a4dcd9e7884f126f6fe8fba5694c1d82acc9460
nandobfer <nandobfer@yahoo.com.br>
2022-12-21 16:57:58 -0300

    feat: added multiple mail processing

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-require('dotenv').config();
-const { readMail } = require('./readEmail');
-const { processEmail } = require('./processEmail');
-const { api } = require('./api');
+require("dotenv").config();
+const { readMail } = require("./readEmail");
+const { processEmail } = require("./processEmail");
+const { api } = require("./api");
 
 const bootstrap = async () => {
-    console.log('Fetching the new mail', new Date().toString());
-    const mail = await readMail();
-    if (!mail) {
-        console.log('There is no unseen e-mail')
+    console.log("Fetching the new mail", new Date().toString());
+    const mails = await readMail();
+    if (!mails[0]) {
+        console.log("There is no unseen e-mail")
         process.exit()
     }
 
-    const { loja, address } = processEmail(mail)
-    console.log(address)
+    console.log(`there are ${mails.length} new orders`)
+    for (const mail of mails) {
+        const { loja, address } = processEmail(mail)
+        console.log({ loja, address })
+    
+        api.post("/pedido", { loja, address })
+            .then((response) => {
+                if (response.data.error) console.error(response.data.error)
+                console.log(response.data)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
 
-    api.post('/pedido', { loja, address })
-        .then((response) => {
-            if (response.data.error) console.error(response.data.error)
-            console.log(response.data)
-        })
-        .catch((error) => {
-            console.error(error)
-        })
 };
 
 bootstrap();
--- readEmail.js
+++ readEmail.js
@@ -3,37 +3,39 @@ const { convert } = require('html-to-text');
 const { READ_MAIL_CONFIG } = require('./config');
 
 const readMail = async () => {
-    const mail = {subject: null, body: null}
   try {
-      const connection = await imaps.connect(READ_MAIL_CONFIG);
-      console.log('CONNECTION SUCCESSFUL', new Date().toString());
-      const box = await connection.openBox('INBOX');
-      const searchCriteria = ['UNSEEN'];
-      const fetchOptions = {
-          bodies: ['HEADER', 'TEXT'],
-          markSeen: true,
-        };
+    const mails = []
+    const connection = await imaps.connect(READ_MAIL_CONFIG);
+    console.log('CONNECTION SUCCESSFUL', new Date().toString());
+    const box = await connection.openBox('INBOX');
+    const searchCriteria = ['UNSEEN'];
+    const fetchOptions = {
+        bodies: ['HEADER', 'TEXT'],
+        markSeen: true,
+    };
 
     const results = await connection.search(searchCriteria, fetchOptions);
 
     if (!results[0]) return false
         
     results.forEach((res) => {
-      const text = res.parts.filter((part) => {
-        return part.which === 'TEXT';
-      });
-      const subject = res.parts.filter((part) => {
-        return part.which === 'HEADER';
-      })[0].body.subject[0];
-      let emailHTML = text[0].body;
-      let emailText = convert(emailHTML);
+        const mail = {subject: null, body: null}
+        const text = res.parts.filter((part) => {
+             return part.which === 'TEXT';
+        });
+        const subject = res.parts.filter((part) => {
+            return part.which === 'HEADER';
+        })[0].body.subject[0];
+        let emailHTML = text[0].body;
+        let emailText = convert(emailHTML);
 
-      mail.subject = subject
-      mail.body = emailText
+        mail.subject = subject
+        mail.body = emailText
+        mails.push(mail)
     });
     
     connection.end();
-    return mail
+    return mails
 
   } catch (error) {
     console.log(error);
